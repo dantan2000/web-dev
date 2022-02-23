04cac529f54ded27dd4d749acd0a96253004ad90
dantan2000 <tan.da@husky.neu.edu>
2022-02-23 09:44:13 -0500

    Created new explore page and started on NavigationSidebar

---
+++ public/tuiter/NavigationSidebar/navElements.js
@@ -0,0 +1,16 @@
+export default [{
+    icon: '',
+    label: '',
+    link: '',
+  },
+  {
+    icon: '',
+    label: '',
+    link: '',
+  },
+  {
+    icon: '',
+    label: '',
+    link: '',
+  },
+];
